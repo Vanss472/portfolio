cddd9eb872fa6fe14a41a5e9f140efa4c1818b4a
Vanessa Martinez <vmartinez@digitalpulp.com>
2019-01-17 17:37:54 -0500

    fixed image path

--- webpack.config.js
+++ webpack.config.js
@@ -80,7 +80,6 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
-              outputPath: 'images/',
               useRelativePath: true
             }
           }
