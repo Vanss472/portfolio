9b047b739a36a1d325c66bc72571454d8848a4fb
Vanessa Martinez <vmartinez@digitalpulp.com>
2019-01-17 17:47:20 -0500

    test this

--- webpack.config.js
+++ webpack.config.js
@@ -80,6 +80,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: '[name].[ext]',
+              outputPath: 'images/',
               useRelativePath: true
             }
           }
