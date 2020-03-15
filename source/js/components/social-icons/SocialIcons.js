c8fd5a0fa41fde7597c0bac07334c8956f3f4847
Vanessa <vanss472@gmail.com>
2020-03-15 11:07:04 -0400

    added aria label to social icons

--- source/js/components/social-icons/SocialIcons.js
+++ source/js/components/social-icons/SocialIcons.js
@@ -6,10 +6,10 @@ const iconPath = './images/';
 
 const SocialIcons = () => {
   const socials = [
-    { profile: "LinkedIn", src: "https://www.linkedin.com/in/vanessa-martinez-7921bb15", icon: "linkedin" },
-    { profile: "GitHub", src: "https://github.com/Vanss472?tab=repositories", icon: "github" },
-    { profile: "CodePen", src: "https://codepen.io/vanss472/pens/popular/", icon: "codepen" },
-    { profile: "Instagram", src: "https://www.instagram.com/vanss472/", icon: "instagram" }
+    { profile: "LinkedIn", src: "https://www.linkedin.com/in/vanessa-martinez-7921bb15", icon: "linkedin", aria: "Visit my LinkedIn profile" },
+    { profile: "GitHub", src: "https://github.com/Vanss472?tab=repositories", icon: "github", aria: "Visit my GitHub profile" },
+    { profile: "CodePen", src: "https://codepen.io/vanss472/pens/popular/", icon: "codepen", aria: "Visit my Codepen playground" },
+    { profile: "Instagram", src: "https://www.instagram.com/vanss472/", icon: "instagram", aria: "Visit my Instagram profile" }
   ];
 
   return (
@@ -19,7 +19,7 @@ const SocialIcons = () => {
           {
             socials.map((social) => (
               <li key={social.profile}>
-                <Link href={social.src}>
+                <Link href={social.src} ariaLabel={social.aria}>
                   <svg>
                     <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref={`${iconPath}${social.icon}.svg#${social.icon}`}></use>
                   </svg>
